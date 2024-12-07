3e8a96335667360a6d3c9822415fcf4cf6c193a3
Aryan Kumar <aryankumar.55566@gmail.com>
2024-12-07 20:44:05 +0530

    a

--- src/components/AddQuestion/AddQuestion.jsx
+++ src/components/AddQuestion/AddQuestion.jsx
@@ -18,22 +18,3 @@ function AddQuestion() {
 
 export default AddQuestion
 
-import SideBar from "../SideBar/SideBar"
-import Feed from "../Feed/Feed"
-
-
-function AddQuestion() {
-
-
-  return (
-    <div className="quora__contents">
-        <div className="quora__content">
-          <SideBar />
-          <Feed/>
-        </div>
-      </div>
-   
-  )
-}
-
-export default AddQuestion
