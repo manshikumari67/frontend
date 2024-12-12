3082559f5cb2b2a07002dbd55544570af347f712
Aryan Kumar <aryankumar.55566@gmail.com>
2024-12-12 15:13:34 +0530

    added jwt-token verification

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -186,7 +186,7 @@ export default function Contact() {
                            <button
                                 type="submit"
                                 className="md:w-32 bg-orange-700 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-orange-600 transition ease-in-out duration-300"
-                                onClick={() => alert("Registered Succesfully")}
+                                onClick={() => alert("Request Sent Successfully!")}
                             >
                                 Submit
                             </button> 
